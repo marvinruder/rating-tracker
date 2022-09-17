732986a064c55354a9b0916bd72b7efa2af19826
Marvin A. Ruder <ruder.marvin@icloud.com>
2022-09-17 15:38:00 +0200

    Handle Redis Client errors (#14)

--- packages/rating-tracker-backend/src/redis/Client.ts
+++ packages/rating-tracker-backend/src/redis/Client.ts
@@ -1,14 +1,20 @@
+import chalk from "chalk";
 import dotenv from "dotenv";
+import { createClient } from "redis";
 import { Client } from "redis-om";
 
 dotenv.config({
   path: ".env.local",
 });
 
-/* pulls the Redis URL from .env */
 const url = process.env.REDIS_URL || "redis://redis:6379";
 
-/* create and open the Redis OM Client */
-const client = await new Client().open(url);
+const redis = createClient({ url: url });
+redis.on("error", (err) =>
+  console.error(chalk.redBright(`Redis Client: ${err}`))
+);
+await redis.connect();
+
+const client = await new Client().use(redis);
 
 export default client;
