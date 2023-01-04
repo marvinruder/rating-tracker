1faf505b3ca9a6a62d357570e79e5162ffde427f
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-01-04 20:23:21 +0100

    Use inline CSS for fonts (#131)
    * Improve routing

--- packages/rating-tracker-frontend/src/index.tsx
+++ packages/rating-tracker-frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import "./fonts.css";
 import "nprogress/nprogress.css";
 import App from "./App";
 import { SidebarProvider } from "./contexts/SidebarContext";
