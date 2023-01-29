6049fa54e2eb6949e73e2e95652c427e286ca671
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-01-29 18:51:12 +0100

    Ignore coverage

--- packages/rating-tracker-backend/src/redis/repositories/user/userRepository.ts
+++ packages/rating-tracker-backend/src/redis/repositories/user/userRepository.ts
@@ -138,6 +138,7 @@ export const deleteUser = async (email: string) => {
     const name = new User(userEntity).name;
     await remove(userEntity.entityId);
     logger.info(PREFIX_REDIS + `Deleted user “${name}” (email ${email}).`);
+    /* istanbul ignore next */ // Not reached in current tests since a user can only delete themself
   } else {
     /* istanbul ignore next */ // Not reached in current tests since a user can only delete themself
     throw new APIError(404, `User ${email} not found.`);
