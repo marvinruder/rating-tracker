de001dd2deea7d40b2d4b970a19587ca28143084
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-02-02 14:03:48 +0100

    Fix https://github.com/facebook/regenerator/issues/378

--- packages/rating-tracker-frontend/src/App.tsx
+++ packages/rating-tracker-frontend/src/App.tsx
@@ -8,6 +8,12 @@ import { NotificationProvider } from "./contexts/NotificationContext";
 import NotificationSnackbar from "./components/NotificationSnackbar";
 
 /**
+ * The globalThis.regeneratorRuntime = undefined addresses a potentially unsafe-eval problem
+ * Source: https://github.com/facebook/regenerator/issues/378#issuecomment-802628326
+ */
+globalThis.regeneratorRuntime = undefined;
+
+/**
  * The Rating Tracker Application.
  *
  * @returns {JSX.Element} The component.
