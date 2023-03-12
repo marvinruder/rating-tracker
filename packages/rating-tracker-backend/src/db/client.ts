0ecf6cf40a21c5e1db9585f0dc607c0cbac25343
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-03-12 17:18:18 +0100

    Try a different approach for importing Prisma Client

--- packages/rating-tracker-backend/src/db/client.ts
+++ packages/rating-tracker-backend/src/db/client.ts
@@ -1,6 +1,7 @@
 // This import works both in dev (with ts-node) and in prod (with precompiled JavaScript and node)
-import Prisma, * as PrismaScope from "../../prisma/client/index.js";
-const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
+import * as PrismaScope from "../../prisma/client/index.js";
+console.log(JSON.stringify(PrismaScope));
+const PrismaClient = PrismaScope.PrismaClient;
 
 /**
  * The Prisma Client. Used for connecting to the database.
