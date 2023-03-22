159433df4ca92c2a1c5333714c26a33ca630ec62
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-03-22 21:09:42 +0100

    Sort stocks in the frontend
    * Request all matching stocks from the backend
    * Sort stocks to the top if their ticker starts with the search value
    * Slice the result array to show the fist 10 results

--- packages/rating-tracker-frontend/src/layouts/SidebarLayout/Header/Buttons/Search/index.tsx
+++ packages/rating-tracker-frontend/src/layouts/SidebarLayout/Header/Buttons/Search/index.tsx
@@ -199,11 +199,20 @@ const HeaderSearch = (): JSX.Element => {
         params: {
           name: currentSearchValue,
           sortBy: "ticker",
-          count: 10, // Only request the first 10 results
         },
       })
       .then((res) => {
-        setStocks(res.data.stocks);
+        const upperCaseSearchValue = currentSearchValue.toLocaleUpperCase();
+        setStocks(
+          res.data.stocks
+            .sort(
+              // Sort stocks to the top if their ticker starts with the search value (case-insensitive)
+              (a: Stock, b: Stock) =>
+                +b.ticker.toLocaleUpperCase().startsWith(upperCaseSearchValue) -
+                +a.ticker.toLocaleUpperCase().startsWith(upperCaseSearchValue)
+            )
+            .slice(0, 10) // Only use the first 10 results
+        );
         setCount(res.data.count);
       })
       .catch((e) => {
