eafc57ec892c9ba02c2667821978d988306926f0
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-05-20 19:35:01 +0200

    Cleanup

--- packages/backend/src/utils/packageInfo-no-assert.ts
+++ packages/backend/src/utils/packageInfo-no-assert.ts
@@ -1,2 +1,6 @@
 import packageInfo from "../../package.json";
+
+/**
+ * The information from `package.json` without an import assertion, which is currently not supported in Vitest.
+ */
 export default packageInfo;
--- packages/backend/src/utils/version-assert.ts
+++
@@ -1,7 +0,0 @@
-import packageInfo from "../../package.json" assert { type: "json" };
-
-/**
- * The version number of the project. It is imported from `package.json` with an import assertion, which is required
- * by Node.
- */
-export const version = packageInfo.version;
