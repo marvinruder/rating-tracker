a5ff7119b1cc2ac30bc719a8f8cc2a58df3af372
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-05-24 15:49:16 +0200

    Inline worker for testing purposes

--- packages/frontend/src/components/ProfileSettings/index.tsx
+++ packages/frontend/src/components/ProfileSettings/index.tsx
@@ -32,7 +32,7 @@ import {
   subscriptionOfMessageType,
   userEndpointPath,
 } from "@rating-tracker/commons";
-import ConvertAvatarWorker from "../../utils/imageManipulation?worker";
+import ConvertAvatarWorker from "../../utils/imageManipulation?worker&inline";
 
 /**
  * A dialog to edit the user’s own information.
