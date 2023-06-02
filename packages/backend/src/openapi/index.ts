34e32404c2dbfa86867089ef3a9f590047ebeb81
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-06-02 21:27:00 +0200

    Release v2.0.0 (#285)

--- packages/backend/src/openapi/index.ts
+++ packages/backend/src/openapi/index.ts
@@ -55,7 +55,7 @@ export const openapiDocument: OpenAPIV3.Document = {
   openapi: "3.0.0",
   info: {
     title: "Rating Tracker Backend",
-    version: "1.0.2",
+    version: "2.0.0",
     contact: {
       name: "Marvin A. Ruder",
       email: "ratingtracker@mruder.dev",
