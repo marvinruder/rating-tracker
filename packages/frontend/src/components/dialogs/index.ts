6a7a6731f3cf56a383b8e6eba6d12d5a7eac5e22
Marvin A. Ruder <ruder.marvin@icloud.com>
2023-06-09 18:35:40 +0200

    Simplify dialogs export

--- packages/frontend/src/components/dialogs/index.ts
+++ packages/frontend/src/components/dialogs/index.ts
@@ -1,21 +1,9 @@
-import { AddStock } from "./AddStock/AddStock";
-import { AddStockToWatchlist } from "./AddStockToWatchlist/AddStockToWatchlist";
-import { AddWatchlist } from "./AddWatchlist/AddWatchlist";
-import { DeleteStock } from "./DeleteStock/DeleteStock";
-import { DeleteUser } from "./DeleteUser/DeleteUser";
-import { DeleteWatchlist } from "./DeleteWatchlist/DeleteWatchlist";
-import { EditStock } from "./EditStock/EditStock";
-import { RemoveStockFromWatchlist } from "./RemoveStockFromWatchlist/RemoveStockFromWatchlist";
-import { RenameWatchlist } from "./RenameWatchlist/RenameWatchlist";
-
-export {
-  AddStock,
-  AddStockToWatchlist,
-  AddWatchlist,
-  DeleteStock,
-  DeleteUser,
-  DeleteWatchlist,
-  EditStock,
-  RemoveStockFromWatchlist,
-  RenameWatchlist,
-};
+export { AddStock } from "./AddStock/AddStock";
+export { AddStockToWatchlist } from "./AddStockToWatchlist/AddStockToWatchlist";
+export { AddWatchlist } from "./AddWatchlist/AddWatchlist";
+export { DeleteStock } from "./DeleteStock/DeleteStock";
+export { DeleteUser } from "./DeleteUser/DeleteUser";
+export { DeleteWatchlist } from "./DeleteWatchlist/DeleteWatchlist";
+export { EditStock } from "./EditStock/EditStock";
+export { RemoveStockFromWatchlist } from "./RemoveStockFromWatchlist/RemoveStockFromWatchlist";
+export { RenameWatchlist } from "./RenameWatchlist/RenameWatchlist";
