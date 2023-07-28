52801d0af34078aedb13f0120711cd4c9f6c754a
Marvin A. Ruder <signed@mruder.dev>
2023-07-28 20:26:49 +0200

    Remove duplicate code related to JSON import assertion (#326)
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/backend/src/utils/packageInfo-assert.ts
+++
@@ -1,6 +0,0 @@
-import packageInfo from "../../package.json" assert { type: "json" };
-
-/**
- * The information from `package.json` with an import assertion, which is required by Node.
- */
-export default packageInfo;
