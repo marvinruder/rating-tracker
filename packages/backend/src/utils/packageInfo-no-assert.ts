ece7c7d481728c81ebd8baa39ad78d98be1872f3
Marvin A. Ruder <signed@mruder.dev>
2023-07-28 20:23:13 +0200

    Remove duplicate code related to JSON import assertion
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/backend/src/utils/packageInfo-no-assert.ts
+++
@@ -1,6 +0,0 @@
-import packageInfo from "../../package.json";
-
-/**
- * The information from `package.json` without an import assertion, which is currently not supported in Vitest.
- */
-export default packageInfo;
--- packages/backend/src/utils/startup.ts
+++ packages/backend/src/utils/startup.ts
@@ -1,3 +1,4 @@
+import packageInfo from "../../package.json" assert { type: "json" };
 import chalk from "chalk";
 import dotenv from "dotenv";
 
@@ -43,11 +44,7 @@ const logo = chalk.bold(
                             ΅΅΅  ΅ΨΪΈώ
                                     ΫΆ΅
 
- Welcome to Rating Tracker v${
-   // Vitest does not support parsing import assertions which are required by Node for JSON imports, so we need to get
-   // creative here
-   (await import(`./packageInfo-${process.env.NODE_ENV === "test" ? "no-assert" : "assert.js"}`)).default.version
- } (${process.env.NODE_ENV ?? "no specific"} environment)!
+ Welcome to Rating Tracker v${packageInfo.version} (${process.env.NODE_ENV ?? "no specific"} environment)!
 
 `),
 );
