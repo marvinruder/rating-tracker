c363c9d512b59e439697eac32916487e4c2a7675
Marvin A. Ruder <signed@mruder.dev>
2023-12-14 19:21:43 +0000

    Add shebang and double quotes to migration script
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/backend/test/all_migrations.sh
+++ packages/backend/test/all_migrations.sh
@@ -1,1 +1,2 @@
-find /docker-entrypoint-initdb.d/migrations -name migration.sql -print0 | sort -z | xargs -0 cat | psql -U $POSTGRES_USER -d $POSTGRES_DB
+#!/bin/ash
+find /docker-entrypoint-initdb.d/migrations -name "migration.sql" -print0 | sort -z | xargs -0 cat | psql -U $POSTGRES_USER -d $POSTGRES_DB
