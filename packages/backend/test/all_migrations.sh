82e64c67df9b48081a09555b78b50509caebd3c6
Marvin A. Ruder <signed@mruder.dev>
2023-12-14 19:25:13 +0000

    Add double quotes to migration script
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/backend/test/all_migrations.sh
+++ packages/backend/test/all_migrations.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-find /docker-entrypoint-initdb.d/migrations -name "migration.sql" -print0 | sort -z | xargs -0 cat | psql -U $POSTGRES_USER -d $POSTGRES_DB
+find /docker-entrypoint-initdb.d/migrations -name migration.sql -print0 | sort -z | xargs -0 cat | psql -U "$POSTGRES_USER" -d "$POSTGRES_DB"
