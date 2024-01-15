7ad1ee574538093ce93e098adfb5d05ffb55ecb4
Marvin A. Ruder <signed@mruder.dev>
2024-01-15 23:23:40 +0100

    Fix misaligned label of 52 week range slider (#899)
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/frontend/src/components/stock/properties/Range52WSlider.tsx
+++ packages/frontend/src/components/stock/properties/Range52WSlider.tsx
@@ -1,5 +1,5 @@
 import type { SliderProps } from "@mui/material";
-import { Slider, useTheme } from "@mui/material";
+import { Slider, SliderMarkLabel, useTheme } from "@mui/material";
 import { FC } from "react";
 
 /**
@@ -13,26 +13,26 @@ export const Range52WSlider: FC<SliderProps> = (props: SliderProps): JSX.Element
   return (
     <Slider
       {...props}
+      slots={{
+        mark: () => undefined, // no marks
+        markLabel: (props) => {
+          const style = props.style ?? {};
+          style.top = 18;
+          // Align the labels based directly on the value
+          const position = Number(props.style.left.replace("%", ""));
+          if (position <= Number.EPSILON) style.transform = "translateX(0%)";
+          if (position >= 100 * (1 - Number.EPSILON)) style.transform = "translateX(-100%)";
+          return <SliderMarkLabel {...props} style={style} />;
+        },
+      }}
       sx={{
         ...props.sx,
-        "@media (pointer: coarse)": {
-          padding: "13px 0",
-        },
+        "@media (pointer: coarse)": { padding: "13px 0" },
         "& .MuiSlider-valueLabel": {
           fontSize: theme.typography.body2.fontSize,
           top: 0,
           backgroundColor: "unset",
           color: theme.palette.text.primary,
-        },
-        "& .MuiSlider-mark": {
-          display: "none",
-        },
-        "& .MuiSlider-markLabel": {
-          top: 18,
-          transform: "translateX(-100%)",
-          "&Active": {
-            transform: "translateX(0%)",
-          },
         },
       }}
     />
