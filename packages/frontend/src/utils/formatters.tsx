fe73720f884f1aa6229e0a1d799b805054a4d1e8
Marvin A. Ruder <signed@mruder.dev>
2024-05-24 15:13:03 +0000

    Fix Market Cap formatting
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/frontend/src/utils/formatters.tsx
+++ packages/frontend/src/utils/formatters.tsx
@@ -9,13 +9,13 @@ import { currencyMinorUnits, currencyName } from "@rating-tracker/commons";
  */
 export const formatMarketCap = (stock: OmitDynamicAttributesStock): string => {
   if (stock.marketCap === null || stock.currency === null) return "–";
-  if (stock.marketCap > 1e12) {
+  if (stock.marketCap >= 1e12) {
     return (stock.marketCap / 1e12).toPrecision(3) + " T"; // trillion, rounded to 3 significant digits
-  } else if (stock.marketCap > 1e9) {
+  } else if (stock.marketCap >= 1e9) {
     return (stock.marketCap / 1e9).toPrecision(3) + " B"; // billion, rounded to 3 significant digits
-  } else if (stock.marketCap > 1e6) {
+  } else if (stock.marketCap >= 1e6) {
     return (stock.marketCap / 1e6).toPrecision(3) + " M"; // million, rounded to 3 significant digits
-  } else if (stock.marketCap > 1e3) {
+  } else if (stock.marketCap >= 1e3) {
     return (stock.marketCap / 1e3).toPrecision(3) + " k"; // thousand, rounded to 3 significant digits
   } else {
     return stock.marketCap.toFixed(0); // rounded to 0 decimal places
