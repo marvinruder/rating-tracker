faf25d913923a888428ef95c106cd51741484d68
Marvin A. Ruder <signed@mruder.dev>
2024-07-27 23:17:36 +0200

    Use PostgreSQL instance from native Alpine package instead of Docker-in-Docker (#1476)
    * Use default user, password, database
    * Drop `security.insecure` flag of build(kit) commands
    
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- packages/backend/test/env.ts
+++ packages/backend/test/env.ts
@@ -1,14 +1,10 @@
 process.env = {
   ...process.env,
-  DATABASE_URL: `postgresql://rating-tracker-test:rating-tracker-test@${
-    process.env.POSTGRES_HOST || "postgres-test"
-  }:5432/rating-tracker-test`,
+  DATABASE_URL: `postgresql://postgres:postgres@${process.env.POSTGRES_HOST ?? "postgres-test"}:5432/postgres`,
   NODE_ENV: "test",
   DOMAIN: "example.com",
   SUBDOMAIN: "subdomain",
   PORT: "30001",
-  POSTGRES_USER: "rating-tracker-test",
-  POSTGRES_PASS: "rating-tracker-test",
   SIGNAL_URL: "http://nonexisting.signal.api.host",
   SIGNAL_SENDER: "+493012345678",
 };
