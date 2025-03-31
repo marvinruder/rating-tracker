e6da664a0a3b194cedb93c0cb77e232f30ff9a95
Marvin A. Ruder <signed@mruder.dev>
2025-03-31 20:13:49 +0000

    Implement ISO 4217 amendments
    * Implement amendment number 176
    * Implement amendment number 177
    * Implement amendment number 178
    
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,5 +3,6 @@ corepack enable
 yarn
 yarn tools
 yarn sdks base
+yarn workspace @rating-tracker/backend build:types
 yarn build:wasm
 yarn validate || :
---
+++ packages/backend/prisma/migrations/0c-iso-4217-amendments/migration.sql
@@ -0,0 +1,9 @@
+-- AlterEnum
+BEGIN;
+CREATE TYPE "currency_new" AS ENUM ('AED', 'AFN', 'ALL', 'AMD', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BOV', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLF', 'CLP', 'CNY', 'COP', 'COU', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GBX', 'GEL', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HTG', 'HUF', 'IDR', 'ILS', 'INR', 'IQD', 'IRR', 'ISK', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MXV', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SOS', 'SRD', 'SSP', 'STN', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'USN', 'UYI', 'UYU', 'UYW', 'UZS', 'VED', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XCG', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWG');
+ALTER TABLE "stocks" ALTER COLUMN "currency" TYPE "currency_new" USING ("currency"::text::"currency_new");
+ALTER TABLE "portfolios" ALTER COLUMN "currency" TYPE "currency_new" USING ("currency"::text::"currency_new");
+ALTER TYPE "currency" RENAME TO "currency_old";
+ALTER TYPE "currency_new" RENAME TO "currency";
+DROP TYPE "currency_old";
+COMMIT;
